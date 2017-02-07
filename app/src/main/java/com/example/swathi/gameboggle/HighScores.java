83760020167a9e3ee756545b0143f70eb6fca1d6
sh <siegby@yahoo.com>
2017-02-06 16:17:18 -0800

    added a new screen for High Scores, added a button to link to high scores on the Main page

---
+++ app/src/main/java/com/example/swathi/gameboggle/HighScores.java
@@ -0,0 +1,18 @@
+package com.example.swathi.gameboggle;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by siggy on 2/6/17.
+ */
+
+public class HighScores extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_high_scores);
+    }
+
+}
