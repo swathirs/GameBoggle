d02e7175e97529720a0c905b42937b15f9a66750
swathirs <rswathi@pdx.edu>
2017-02-14 22:32:17 -0800

    updated Dictionary class.

--- app/src/main/java/com/example/swathi/gameboggle/Dictionary.java
+++ app/src/main/java/com/example/swathi/gameboggle/Dictionary.java
@@ -3,58 +3,53 @@ package com.example.swathi.gameboggle;
 /**
  * Created by John on 2/10/2017.
  */
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Context;
+import android.util.Log;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.*;
 
-public class Dictionary extends AppCompatActivity {
-    public ArrayList<String> dic = new ArrayList<String>();
-    public  ArrayList<String> wordlist = new ArrayList<String>();
-    public  static ArrayList<String> solution = new ArrayList<String>();
-    public  static ArrayList<String> answer = new ArrayList<String>();
 
-    public StringBuilder buf = new StringBuilder();
-    public String[] dictWords;
+public class Dictionary {
+    private Context context;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_third_screen);
+    public Dictionary(Context context) {
+        this.context = context;
 
-        wordlist = new ArrayList<String>();
-        solution = new ArrayList<String>();
-        answer = new ArrayList<String>();
-
-        InputStream in = getResources().openRawResource(getResources().getIdentifier("dictionary", "raw", getPackageName()));
-        if (in != null) {
-            InputStreamReader tmp = new InputStreamReader(in);
-            BufferedReader reader = new BufferedReader(tmp);
-            String str;
-
-            try {
-                while ((str = reader.readLine()) != null) {
-                    buf.append(str.toUpperCase() + " ");
-                    dic.add(str.toUpperCase());
-
-                }
-            } catch (IOException e) {
-
-            }
-            try {
-                in.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            dictWords = buf.toString().split(" ");
-
-        }
+        if(LoadWordsFile() == -1){
+            System.out.println("Could not find file 'dictionary.txt'.");
+            return;
+        };
 
     }
 
+    private int LoadWordsFile() {
+
+        try {
+            InputStream in = context.getResources().openRawResource(context.getResources().getIdentifier("words", "raw", context.getPackageName()));
+            /*InputStream inputStream = context.getResources().openRawResource(R.raw.bbwords);
+            //BufferedReader buffReader = new BufferedReader(new FileReader(BBValidWordsFile));
+            BufferedReader buffReader = new BufferedReader(new InputStreamReader(inputStream));*/
+
+
+                InputStreamReader tmp = new InputStreamReader(in);
+                BufferedReader reader = new BufferedReader(tmp);
+
+
+            reader.close();
+
+        } catch (IOException e) {
+            Log.e("LoadWordsFile():", "Exception occurred while trying to open the valid words file. \n");
+            //e.printStackTrace();
+            return -1;
+        }
+        return 1;
+    }
 
 }
