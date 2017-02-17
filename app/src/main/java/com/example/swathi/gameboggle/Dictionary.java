18e09c6a362bbc47d90ef9528dbf255c0704093d
rip3 <rip3@pdx.edu>
2017-02-17 13:25:36 -0800

    Added Trie to Dictionary

--- app/src/main/java/com/example/swathi/gameboggle/Dictionary.java
+++ app/src/main/java/com/example/swathi/gameboggle/Dictionary.java
@@ -14,13 +14,17 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.*;
+import java.lang.Character;
 
 
 public class Dictionary {
     private Context context;
+    private TrieNode root;
 
     public Dictionary(Context context) {
         this.context = context;
+
+        root = new TrieNode(false);
 
         if(LoadWordsFile() == -1){
             System.out.println("Could not find file 'dictionary.txt'.");
@@ -41,12 +45,12 @@ public class Dictionary {
                 InputStreamReader tmp = new InputStreamReader(in);
                 BufferedReader reader = new BufferedReader(tmp);
                 String str;
-           /* while ((str = reader.readLine()) != null) {
+                while ((str = reader.readLine()) != null) {
 
-                     //   ValidWords logic
+                     this.addWord(str);
 
 
-            } */
+                }
 
 
             reader.close();
@@ -58,5 +62,123 @@ public class Dictionary {
         }
         return 0;
     }
+
+    private void addWord(String word)
+    {
+        char letter = ' ';
+        TrieNode currentNode = null;
+
+        if (word.length() < 1)
+            return;
+
+        currentNode = root;
+        for (int i = 0; i < word.length(); i++)
+        {
+            letter = word.charAt(i);
+            if (currentNode.traverseChar(letter) == null)
+                currentNode.addChild(letter);
+            currentNode = currentNode.traverseChar(letter);
+        }
+        currentNode.markWord(true);
+    }
+
+    public boolean isWord(String word)
+    {
+        char letter = ' ';
+        TrieNode currentNode = null;
+
+        if (word.length() < 1)
+            return false;
+
+        currentNode = root;
+        for (int i = 0; i < word.length(); i++)
+        {
+            letter = word.charAt(i);
+            if (currentNode.traverseChar(letter) == null)
+                return false;
+            currentNode = currentNode.traverseChar(letter);
+        }
+
+        return currentNode.isWord();
+    }
+
+    public boolean isPrefix(String word)
+    {
+        char letter = ' ';
+        TrieNode currentNode = null;
+
+        if (word.length() < 1)
+            return true;
+
+        currentNode = root;
+        for (int i = 0; i < word.length(); i++)
+        {
+            letter = word.charAt(i);
+            if (currentNode.traverseChar(letter) == null)
+                return false;
+            currentNode = currentNode.traverseChar(letter);
+        }
+
+        return true;
+    }
+
+    private class TrieNode
+    {
+        private TrieNode next[];
+        private boolean isWord;
+        //private char letter;
+
+        TrieNode()
+        {
+            next = new TrieNode[26];
+            for (int i = 0; i < 26; i++)
+            {
+                next[i] = null;
+            }
+            isWord = false;
+            //letter = ' ';
+        }
+
+        TrieNode(boolean isWord)
+        {
+            next = new TrieNode[26];
+            for (int i = 0; i < 26; i++)
+            {
+                next[i] = null;
+            }
+            this.isWord = isWord;
+            //this.letter = letter;
+        }
+
+        public boolean addChild(char letter)
+        {
+            char lowercase = Character.toLowerCase(letter);
+            if (this.next[lowercase] != null)
+                return false;
+            this.next[lowercase] = new TrieNode();
+            return true;
+        }
+
+        public TrieNode traverseChar(char letter)
+        {
+            return this.next[Character.toLowerCase(letter) - 97];
+        }
+
+        public TrieNode traverseIndex(int index)
+        {
+            return this.next[index];
+        }
+
+        public boolean isWord()
+        {
+            return this.isWord;
+        }
+
+        public void markWord(boolean isWord)
+        {
+            this.isWord = isWord;
+        }
+    }
+
 
 }
