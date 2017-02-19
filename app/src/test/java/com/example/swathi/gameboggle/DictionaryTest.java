4e0acbc82c7d6ea89e1894d74c1d900805525758
sh <siegby@yahoo.com>
2017-02-19 15:58:38 -0800

    tried to mock context - failing test - in progress

--- app/src/test/java/com/example/swathi/gameboggle/DictionaryTest.java
+++ app/src/test/java/com/example/swathi/gameboggle/DictionaryTest.java
@@ -4,48 +4,44 @@ import android.content.Context;
 
 import org.junit.Before;
 import org.junit.Test;
-
 import static org.junit.Assert.*;
-//import static org.junit.Assert.assertFalse;
-//import static org.junit.Assert.assertTrue;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.CoreMatchers.*;
-import static org.mockito.Mockito.*;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
-import android.content.SharedPreferences;
-
 
 
 
 @RunWith(MockitoJUnitRunner.class)
 public class DictionaryTest {
-    private Dictionary aDictionary;
 
-    public DictionaryTest() {
-        aDictionary = new Dictionary(mMockContext);
-    }
+    private Dictionary aDictionary;
 
     @Mock
     Context mMockContext;
+
+    @Before
+    public void setUp() throws Exception {
+        aDictionary = new Dictionary(mMockContext);
+    }
+
 
     @Test
     public void isWord_validWord_ReturnsTrue() {
         assertTrue(aDictionary.isWord("aback"));
     }
 
+
     @Test
     public void isWord_invalidWord_ReturnsFalse() {
         assertFalse(aDictionary.isWord("ab&*2d"));
     }
 
+
     @Test
     public void isWord_emptyString_ReturnsFalse() {
         assertFalse(aDictionary.isWord(""));
     }
+
 
     @Test
     public void isWord_nullWord_ReturnsFalse() {
