9544337fe6987eaabf0ec5e5088c56fd698c2db3
sh <siegby@yahoo.com>
2017-02-18 21:17:05 -0800

    added some new class testing files for unit tests, moved the unit test for gen random Board to dice controller test file

---
+++ app/src/test/java/com/example/swathi/gameboggle/DictionaryTest.java
@@ -0,0 +1,14 @@
+package com.example.swathi.gameboggle;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+
+public class DictionaryTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+}
