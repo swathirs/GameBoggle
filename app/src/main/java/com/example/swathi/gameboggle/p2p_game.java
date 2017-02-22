74c7567ccc7b1ab9dd518d3d7221aa7e6bff26e9
John Donahue <dohnjonahue.bags@gmail.com>
2017-02-22 11:11:02 -0800

    Add basic p2p game activity.
    Added the new activity for a P2P game with the same design as a
    single-player game.

---
+++ app/src/main/java/com/example/swathi/gameboggle/p2p_game.java
@@ -0,0 +1,13 @@
+package com.example.swathi.gameboggle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class p2p_game extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_p2p_game);
+    }
+}
