a4ef71063f0c4c413eddfd557e7a2ac3021426cc
siggy-h <siegby@yahoo.com>
2017-02-28 10:49:21 -0800

    working on validWord test, setting up list of squares with only one valid word.

--- app/src/androidTest/java/com/example/swathi/gameboggle/Instrumented_ValidWordsTest.java
+++ app/src/androidTest/java/com/example/swathi/gameboggle/Instrumented_ValidWordsTest.java
@@ -3,10 +3,7 @@ package com.example.swathi.gameboggle;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
-import android.os.Looper; // LOOPER
-import android.util.Log;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,82 +13,67 @@ import java.util.ArrayList;
 
 @RunWith(AndroidJUnit4.class)
 public class Instrumented_ValidWordsTest {
+    // ValidWords Class Fields:
+    //    private int difficulty;
+    //    private ArrayList<WordNode> validList[];
+    //
+    //    //These variables are used in finding the list of valid words
+    //    private String tempBoard[][];
+    //    private boolean visited[][];
+    //    private int total;
+    //    private int maxLength; //This variable is used to limit words to a specified length.  not meant for final version
+    //
+    //    //Dictionary
+    //    private Dictionary dictionary;
+
+    // Constructor input:   ArrayList<String> squares, int difficulty, Dictionary dictionary
+
 
     private Context appContext;
-
     private Dictionary aDictionary;
-    private DiceController roller;
+    private DiceController roller_good;
+    private DiceController roller_bad;
     private ValidWords aValidWords;
-
-    private Looper mLooper = null;
-
-
-
-    /** Initializes the message looper receive the callback messages. */
-    private void initializeMessageLooper() {
-        // final ConditionVariable startDone = new ConditionVariable();
-
-        new Thread() {
-            @Override
-            public void run() {
-                // Set up a looper to be used
-                Looper.prepare();
-
-                // Save the looper so that we can terminate this thread
-                // after we are done with it.
-                mLooper = Looper.myLooper();
-
-                //mCamera = Camera.open();
-                //startDone.open();
-
-                Looper.loop();  // Blocks forever until Looper.quit() is called.
-
-            }
-        }.start();
-//        if (!startDone.block(WAIT_FOR_COMMAND_TO_COMPLETE)) {
-//            fail("initializeMessageLooper: start timeout");
-//        }
-    }
-
-
-    //   // Initializes ValidWords for a specific board
-    //public ValidWords(ArrayList<String> squares, int difficulty, Dictionary dictionary) {
 
 
     @Before
     public void setUp() throws Exception {
-        // initializeMessageLooper();  //start looper thread
-
         appContext = InstrumentationRegistry.getTargetContext();  // context
         aDictionary = new Dictionary(appContext);                 // a dictionary
 
-        roller = new DiceController();      // dice controller get squares
-        ArrayList<String> squares = roller.genBoard();  // squares
+        roller_good = new DiceController();  // dice controller get squares
+        ArrayList<String> squares_good = roller_good.genBoard();  // squares
+
+        ArrayList<String> squares_bad = create_bad_squares();
+
 
         // Create ValidWords Object:  squares, int difficulty, dictionary
-        aValidWords = new ValidWords(squares, 1, aDictionary);
+        aValidWords = new ValidWords(squares_good, 1, aDictionary);
     }
 
 
+    /**
+     * create_bad_squares(): creates an array of squares with only one word,
+     * this is an invalid boggle board.
+     * Returns ArrayList
+     */
+    public ArrayList<String> create_bad_squares() {
+        //roller_bad = new DiceController();  // dice controller for Bad_Board_Test
 
-    /*
-    *  Terminates the message looper thread.
-    */
-    @After
-    private void terminateMessageLooper() throws Exception {
-//        mLooper.quit();
-        // Looper.quit() is asynchronous. The looper may still has some
-        // preview callbacks in the queue after quit is called. The preview
-        // callback still uses the camera object (setHasPreviewCallback).
+        ArrayList<String> temp = new ArrayList<String>();
+        // add word "and"
+        temp.add("a");
+        temp.add("n");
+        temp.add("d");
+        // add "z" to rest of the board
+        for(int i = 0; i < 9; i++) {
+            temp.add("z");
+        }
 
-        // After camera is released, RuntimeException will be thrown from
-        // the method. So we need to join the looper thread here.
-//        mLooper.getThread().join();
-        //mCamera.release();
+        System.out.print("\n\n bad squares: " + temp + "\n");
+
+        return temp;
     }
-
-
-
 
 
     // tests for Valid Word?
@@ -102,8 +84,55 @@ public class Instrumented_ValidWordsTest {
     }
 
 
-
-
-
-
 } //end class
+
+
+// Notes: TODO remove...
+
+// private Looper mLooper = null;
+
+// ... in @Bbefore
+//         // initializeMessageLooper();  //start looper thread
+
+
+//    /** Initializes the message looper receive the callback messages. */
+//    private void initializeMessageLooper() {
+//        // final ConditionVariable startDone = new ConditionVariable();
+//
+//        new Thread() {
+//            @Override
+//            public void run() {
+//                // Set up a looper to be used
+//                Looper.prepare();
+//
+//                // Save the looper so that we can terminate this thread
+//                // after we are done with it.
+//                mLooper = Looper.myLooper();
+//
+//                //mCamera = Camera.open();
+//                //startDone.open();
+//                Looper.loop();  // Blocks forever until Looper.quit() is called.
+//            }
+//        }.start();
+//        if (!startDone.block(WAIT_FOR_COMMAND_TO_COMPLETE)) {
+//            fail("initializeMessageLooper: start timeout");
+//        }
+//    }
+
+
+
+    /*
+    *  Terminates the message looper thread.
+    */
+//    @After
+//    private void terminateMessageLooper() throws Exception {
+//        mLooper.quit();
+// Looper.quit() is asynchronous. The looper may still has some
+// preview callbacks in the queue after quit is called. The preview
+// callback still uses the camera object (setHasPreviewCallback).
+
+// After camera is released, RuntimeException will be thrown from
+// the method. So we need to join the looper thread here.
+//        mLooper.getThread().join();
+//mCamera.release();
+//    }
