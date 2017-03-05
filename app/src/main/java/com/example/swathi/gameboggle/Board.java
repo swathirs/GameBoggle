f8fad86636dccf55881ac5e10550b25d209ef756
sh <siegby@yahoo.com>
2017-03-04 16:49:36 -0800

    fix bug in checking valid word

--- app/src/main/java/com/example/swathi/gameboggle/Board.java
+++ app/src/main/java/com/example/swathi/gameboggle/Board.java
@@ -46,9 +46,9 @@ public class Board {
 
         while (!valid.checkValidBoard())
         {
+            squares = roller.genBoard();  // get new set of letters
             valid = new ValidWords(squares, difficulty, dictionary);
         }
-
         return true;
     }
 
