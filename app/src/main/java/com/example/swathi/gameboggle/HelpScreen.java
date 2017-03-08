a206ee21d499cfce18d5b4e04460e758880ac892
siggy-h <siegby@yahoo.com>
2017-03-07 22:17:09 -0800

    added radio group and actions that toggle on off the single or multi player help text

--- app/src/main/java/com/example/swathi/gameboggle/HelpScreen.java
+++ app/src/main/java/com/example/swathi/gameboggle/HelpScreen.java
@@ -2,17 +2,28 @@ package com.example.swathi.gameboggle;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class HelpScreen extends AppCompatActivity {
+
+    TextView single_help, multi_help;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help_screen);
+
+        single_help = (TextView) findViewById(R.id.tvSingleHelp);
+        multi_help = (TextView) findViewById(R.id.tvMultiHelp);
+
+        multi_help.setVisibility(View.INVISIBLE);   // hide multi
     }
 
     /** onCreateOptionsMenu: Creates the option menu at top of screen */
@@ -39,6 +50,28 @@ public class HelpScreen extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
         return true;
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radioSingleHelp:
+                if (checked)
+                    single_help.setVisibility(View.VISIBLE);    // show single
+                    multi_help.setVisibility(View.INVISIBLE);   // hide multi
+
+                break;
+            case R.id.radioMultiHelp:
+                if (checked)
+                    single_help.setVisibility(View.INVISIBLE);  // hide single
+                    multi_help.setVisibility(View.VISIBLE);     // show multi
+
+                break;
+
+        }
     }
 
 
